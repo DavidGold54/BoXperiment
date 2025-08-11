a0259a157e5f7a7ba4fa7a35304e8c7b1f879f2b
DavidGold54 <gold1354berg@outlook.jp>
2025-08-11 17:37:43 +0900

    feat: Create core/__init__.py and core/problems.py, and add the implementation of the problem factory.

---
+++ core/problems.py
@@ -0,0 +1,53 @@
+# ============================================================================
+# Problems
+# ============================================================================
+import inspect
+from abc import ABC, abstractmethod
+
+import torch
+from torch import Tensor
+from botorch.test_functions.base import BaseTestProblem
+from botorch.exceptions.errors import InputDataError, UnsupportedError
+from botorch.test_functions import (
+    synthetic, multi_fidelity, multi_objective,
+    multi_objective_multi_fidelity, sensitivity_analysis,
+)
+
+
+# ----------------------------------------------------------------------------
+# Problem Factory
+# ----------------------------------------------------------------------------
+class ProblemFactory:
+
+    REGISTRY: dict[str, BaseTestProblem] = {
+        # Synthetic test functions
+        "Branin" : synthetic.Branin
+    }
+
+    @classmethod
+    def register(cls, name: str):
+        def decorator(problem_cls: BaseTestProblem):
+            cls.REGISTRY[name] = problem_cls
+            return problem_cls
+        return decorator
+
+    @classmethod
+    def create(cls, name: str, **kwargs) -> BaseTestProblem:
+        if name not in cls.REGISTRY:
+            raise ValueError(
+                f"Unsupported problem type: {name}. ",
+                f"Available problems: {list(cls.REGISTRY.keys())}"
+            )
+        problem_cls = cls.REGISTRY[name]
+        sig = inspect.signature(problem_cls.__init__).parameters.values()
+        args_list = [p.name for p in sig if p.name != "self"]
+        params = {}
+        for k, v in kwargs.items():
+            if k in args_list:
+                params[k] = v
+        return problem_cls(**params)
+
+
+# ----------------------------------------------------------------------------
+# Custom Problems
+# ----------------------------------------------------------------------------
