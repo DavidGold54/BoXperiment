b706ac50b61bcca5031652f3ca73c4c8904b9bfd
DavidGold54 <gold1354berg@outlook.jp>
2025-08-14 15:03:51 +0900

    feat: Added a new AcquisitionFactory class and implemented a function to manage the generation of acquisition functions.

---
+++ core/acquisitions.py
@@ -0,0 +1,57 @@
+# ============================================================================
+# Acquisitions
+# ============================================================================
+import inspect
+
+from botorch.models.model import Model
+from botorch.acquisition import AcquisitionFunction
+from botorch.acquisition import (
+    analytic, monte_carlo, knowledge_gradient, max_value_entropy_search,
+    predictive_entropy_search, 
+)
+import botorch.acquisition.multi_objective as mo
+
+
+# ----------------------------------------------------------------------------
+# Acquisition Factory
+# ----------------------------------------------------------------------------
+class AcquisitionFactory:
+    """A factory that generates AcquisitionFunction objects from name strings.
+    """
+
+    REGISTRY: dict[str, AcquisitionFunction] = {
+        "ExpectedImprovement": analytic.ExpectedImprovement,
+    }
+
+    @classmethod
+    def register(cls, name: str):
+        def decorator(acqf_cls: AcquisitionFunction):
+            cls.REGISTRY[name] = acqf_cls
+            return acqf_cls
+        return decorator
+
+    @classmethod
+    def create(
+        cls,
+        name: str,
+        model: Model,
+        **kwargs,
+    ) -> AcquisitionFunction:
+        if name not in cls.REGISTRY:
+            raise ValueError(
+                f"Unsupported acquisition type: {name}. ",
+                f"Available acquisitions: {list(cls.REGISTRY.keys())}"
+            )
+        acqf_cls = cls.REGISTRY[name]
+        sig = inspect.signature(acqf_cls.__init__).parameters.values()
+        args_list = [p.name for p in sig if p.name != "self"]
+        params = {"model": model}
+        for key, value in kwargs.items():
+            if key in args_list:
+                params[key] = value
+        return acqf_cls(**params)
+
+
+# ----------------------------------------------------------------------------
+# Custom Acquisitions
+# ----------------------------------------------------------------------------
