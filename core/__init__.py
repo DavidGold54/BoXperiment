a0faaf84a498c6034a59ecd02e1127b784116982
DavidGold54 <gold1354berg@outlook.jp>
2025-08-14 15:04:29 +0900

    feat: AcquisitionFactory, MLLFactory, ModelFactory, and ProblemFactory were added to the core module, and a public API was defined.

---
+++ core/__init__.py
@@ -0,0 +1,12 @@
+from core.acquisitions import AcquisitionFactory
+from core.model_utils import MLLFactory
+from core.models import ModelFactory
+from core.problems import ProblemFactory
+
+
+__all__ = [
+    "AcquisitionFactory",
+    "MLLFactory",
+    "ModelFactory",
+    "ProblemFactory",
+]
